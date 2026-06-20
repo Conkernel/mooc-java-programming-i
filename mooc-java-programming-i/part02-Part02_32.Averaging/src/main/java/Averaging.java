d902c674a8295386b20fb97721b902b5bd8b6f68
conlernel <conkernel.outlook.com>
2026-06-20 08:05:39 +0200

    Part02_32.Averaging Done

--- mooc-java-programming-i/part02-Part02_32.Averaging/src/main/java/Averaging.java
+++ mooc-java-programming-i/part02-Part02_32.Averaging/src/main/java/Averaging.java
@@ -3,8 +3,7 @@ public class Averaging {
 
     // implement the sum method here again
     public static int sum(int number1, int number2, int number3, int number4) {
-        int suma = number1+number2+number3+number4;
-        return suma;
+        return number1+number2+number3+number4;
     }
 
     public static double average(int number1, int number2, int number3, int number4) {
